76ed514f6f7147ac618d4f14c88c87a96093266f
dalen romelien <dalenm.romelien@gmail.com>
2025-11-25 14:50:02 -0500

    persist changes to step-ca

--- services/step-ca.nix
+++ services/step-ca.nix
@@ -8,15 +8,15 @@
     port = 9000;
 
     settings = {
-	    root = "/etc/smallstep/certs/root_ca.crt";
-	    crt = "/etc/smallstep/certs/intermediate_ca.crt";
-	    key = "/etc/smallstep/secrets/intermediate_ca_key";
+	    root = "/var/lib/step-ca/certs/root_ca.crt";
+	    crt = "/var/lib/step-ca/certs/intermediate_ca.crt";
+	    key = "/var/lib/step-ca/secrets/intermediate_ca_key";
 	    address = ":9000";
 	    dnsNames = ["ca.dalen-homelab.com"];
-	    logger = {format = "text"};
+	    logger = {format = "text";};
 	    db = {
 	      type = "badgerv2";
-        dataSource = "/etc/smallstep/db";
+              dataSource = "/var/lib/smallstep/db";
 	    };
 
 	    authority = {
@@ -35,7 +35,7 @@
 	      tls = {
 	        cipherSuites = ["TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256" "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256"];
 	        minVersion = 1.2;
-          maxVersion = 1.3;
+                maxVersion = 1.3;
 	        renegotiation = false;
 	      };
 	      commonName = "Step Online CA";
